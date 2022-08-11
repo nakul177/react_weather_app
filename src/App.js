5504f918ec1e8909c6f09b457047925e7b0a7299
nakul177 <96129513+nakul177@users.noreply.github.com>
2022-08-12 00:14:25 +0530

    create app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
-import './App.css';
+import "./App.css";
+import TopButton from "./components/TopButton";
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello Nakul </h1>
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+      <TopButton />
     </div>
   );
 }
