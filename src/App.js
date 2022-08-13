dd0c3b518c64e5b3f869192b27575c5d8d3bd9ab
nakul177 <96129513+nakul177@users.noreply.github.com>
2022-08-14 01:50:22 +0530

    complet

--- src/App.js
+++ src/App.js
@@ -7,50 +7,46 @@ import Forecast from "./components/Forecast";
 import getFormattedWeatherData from "./services/weatherService";
 import { useEffect, useState } from "react";
 
-
 function App() {
-
-  const [query , setQurey] = useState({q:"Pune"})
-  const [units , setUnits] = useState("metric")
-  const [weather , setWeather] = useState(null)
-
+  const [query, setQurey] = useState({ q: "Pune" });
+  const [units, setUnits] = useState("metric");
+  const [weather, setWeather] = useState(null);
 
   useEffect(() => {
     const fetchWeather = async () => {
-     getFormattedWeatherData({...query , units}).then((data) => {
-    setWeather(data)
-     });
+      getFormattedWeatherData({ ...query, units }).then((data) => {
+        setWeather(data);
+      });
     };
-  
-    fetchWeather()
-  },[query , units])
 
-  console.log(weather)
+    fetchWeather();
+  }, [query, units]);
 
+  console.log(weather);
 
   const formatBg = () => {
-    if(!weather) return "from-cyan-700 to-blue-700"
-    const threshold = units==="metric" ? 20 : 60 ;
-    if(weather.temp >= threshold) return "from-cyan-700 to-blue-700"
-    return "from-yellow-700 to-orange-700"
-  }
+    if (!weather) return "from-cyan-700 to-blue-700";
+    const threshold = units === "metric" ? 20 : 60;
+    if (weather.temp >= threshold) return "from-cyan-700 to-blue-700";
+
+    return "from-yellow-700 to-orange-700";
+  };
 
   return (
-    <div className={`mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br ${formatBg()} h-fit shadow-xl shadow-gray-400`}>
-
+    <div
+      className={`mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br h-fit shadow-xl shadow-gray-400  ${formatBg()}`}
+    >
       <TopButton />
       <Input setQurey={setQurey} units={units} setUnits={setUnits} />
 
-  {weather&& (
-    <>
-      <TimeAndLocation  weather={weather}/>
-      <TempratureAndDetails  weather={weather}/>
-      <Forecast title={"Hourly Forecast"}  items={weather.hourly}/>
-      <Forecast title={"Daily Forecast"}  items={weather.daily}/>
-    </>
-  )}
-
-    
+      {weather && (
+        <>
+          <TimeAndLocation weather={weather} />
+          <TempratureAndDetails weather={weather} />
+          <Forecast title={"Hourly Forecast"} items={weather.hourly} />
+          <Forecast title={"Daily Forecast"} items={weather.daily} />
+        </>
+      )}
     </div>
   );
 }
